0da82bcfe94db59a7b14c4856678027ba35cc993
nisrinasals <nisrinahikmawan@gmail.com>
2024-03-29 16:41:10 +0700

    merapikan codingan sebelumnya

--- QuickSort_0054.cpp
+++ QuickSort_0054.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int arr[20];
@@ -6,27 +6,30 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
-void input(){
-while (true){
-    cout << "Masukkan panjang element array: ";
-    cin >> n;
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
 
-    if (n<=20)
-    break;
-    else
-    cout << "\nMaksimum panjang array adalah 20" << endl;
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
-cout << "\n-------------------" << endl;
-cout << "\nEnter Array Element" << endl;
-cout << "\n-------------------" << endl;
-
-for (int i=0;i<n;i++){
-cout << "<" << (i+1) << ">";
-cin >> arr[i];
-}
-}
-
-int main(){
-
+int main()
+{
 }
